d241a2211af180485620e5d66dba0039ccda5edd
khizzar.mansoor@norgic.com <khizzar.mansoor@norgic.com>
2023-02-14 19:54:01 +0500

    fixed reconnect and network switch issue

--- app/src/main/java/com/vdotok/one2many/VdoTok.kt
+++ app/src/main/java/com/vdotok/one2many/VdoTok.kt
@@ -1,17 +1,15 @@
 package com.vdotok.one2many
 
 import android.app.Application
-import android.util.Log
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.ProcessLifecycleOwner
-import com.vdotok.one2many.extensions.show
+import com.vdotok.network.models.LoginResponse
 import com.vdotok.one2many.prefs.Prefs
 import com.vdotok.streaming.CallClient
 import com.vdotok.streaming.enums.MediaType
 import com.vdotok.streaming.enums.SessionType
 import com.vdotok.streaming.models.CallParams
-import com.vdotok.streaming.views.CallViewRenderer
 
 
 /**
@@ -20,56 +18,83 @@ import com.vdotok.streaming.views.CallViewRenderer
  */
 class VdoTok : Application() {
 
-   lateinit var callClient: CallClient
-    private lateinit var prefs : Prefs
+    lateinit var callClient: CallClient
+    private lateinit var prefs: Prefs
     var callParam1: CallParams? = null
-    var callParam2:CallParams? = null
-    var camView:Boolean = true
+    var callParam2: CallParams? = null
+    var camView: Boolean = true
     private var lifecycleEventObserver = LifecycleEventObserver { _, event ->
-       when (event) {
-          Lifecycle.Event.ON_RESUME -> {
-              if ((callParam1?.mediaType == MediaType.VIDEO && callParam1?.sessionType == SessionType.CALL) || (callParam2?.mediaType == MediaType.VIDEO && callParam2?.sessionType == SessionType.CALL)) {
-                  if (camView) {
-                      if (callParam1?.sessionType == SessionType.CALL) {
-                          callClient.resumeVideo(
-                              prefs.loginInfo?.refId.toString(),
-                              callParam1?.sessionUUID.toString()
-                          )
-                      } else {
-                          callClient.resumeVideo(
-                              prefs.loginInfo?.refId.toString(),
-                              callParam2?.sessionUUID.toString()
-                          )
-                      }
-                  }else{
-                      if (callParam1?.sessionType == SessionType.CALL){
-                          callClient.pauseVideo(prefs.loginInfo?.refId.toString(),callParam1?.sessionUUID.toString())
-                      }else{
-                          callClient.pauseVideo(prefs.loginInfo?.refId.toString(),callParam2?.sessionUUID.toString())
-                      }
-                  }
-              }
-          }
-           Lifecycle.Event.ON_PAUSE -> {
-               if ((callParam1?.mediaType == MediaType.VIDEO && callParam1?.sessionType == SessionType.CALL) || (callParam2?.mediaType == MediaType.VIDEO && callParam2?.sessionType == SessionType.CALL)) {
-                      if (callParam1?.sessionType == SessionType.CALL){
-                          callClient.pauseVideo(prefs.loginInfo?.refId.toString(),callParam1?.sessionUUID.toString())
-                      }else{
-                          callClient.pauseVideo(prefs.loginInfo?.refId.toString(),callParam2?.sessionUUID.toString())
-                      }
-               }
-              }
+        when (event) {
+            Lifecycle.Event.ON_RESUME -> {
+                connectClient()
+                if ((callParam1?.mediaType == MediaType.VIDEO && callParam1?.sessionType == SessionType.CALL) || (callParam2?.mediaType == MediaType.VIDEO && callParam2?.sessionType == SessionType.CALL)) {
+                    if (camView) {
+                        if (callParam1?.sessionType == SessionType.CALL) {
+                            callClient.resumeVideo(
+                                prefs.loginInfo?.refId.toString(),
+                                callParam1?.sessionUUID.toString()
+                            )
+                        } else {
+                            callClient.resumeVideo(
+                                prefs.loginInfo?.refId.toString(),
+                                callParam2?.sessionUUID.toString()
+                            )
+                        }
+                    } else {
+                        if (callParam1?.sessionType == SessionType.CALL) {
+                            callClient.pauseVideo(
+                                prefs.loginInfo?.refId.toString(),
+                                callParam1?.sessionUUID.toString()
+                            )
+                        } else {
+                            callClient.pauseVideo(
+                                prefs.loginInfo?.refId.toString(),
+                                callParam2?.sessionUUID.toString()
+                            )
+                        }
+                    }
+                }
+            }
+            Lifecycle.Event.ON_PAUSE -> {
+                if ((callParam1?.mediaType == MediaType.VIDEO && callParam1?.sessionType == SessionType.CALL) || (callParam2?.mediaType == MediaType.VIDEO && callParam2?.sessionType == SessionType.CALL)) {
+                    if (callParam1?.sessionType == SessionType.CALL) {
+                        callClient.pauseVideo(
+                            prefs.loginInfo?.refId.toString(),
+                            callParam1?.sessionUUID.toString()
+                        )
+                    } else {
+                        callClient.pauseVideo(
+                            prefs.loginInfo?.refId.toString(),
+                            callParam2?.sessionUUID.toString()
+                        )
+                    }
+                }
+            }
             else -> {}
-         }
-     }
+        }
+    }
 
-   override fun onCreate() {
-       super.onCreate()
-       vdotok = this
-       callClient = CallClient.getInstance(this)!!
-       prefs = Prefs(this)
-       ProcessLifecycleOwner.get().lifecycle.addObserver(lifecycleEventObserver)
-   }
+    private fun connectClient() {
+        prefs.loginInfo?.mediaServer?.let {
+            if (!callClient.isConnected())
+                callClient.connect(
+                    getMediaServerAddress(it),
+                    it.endPoint
+                )
+        }
+    }
+
+    private fun getMediaServerAddress(mediaServer: LoginResponse.MediaServerMap): String {
+        return "https://${mediaServer.host}:${mediaServer.port}"
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        vdotok = this
+        callClient = CallClient.getInstance(this)!!
+        prefs = Prefs(this)
+        ProcessLifecycleOwner.get().lifecycle.addObserver(lifecycleEventObserver)
+    }
 
     companion object {
         private var vdotok: VdoTok? = null
